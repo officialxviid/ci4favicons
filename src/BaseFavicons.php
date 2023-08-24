9d807ee229bebd06e1d5fc00ede21ae25d6450fe
XVIID <developer@xviid.net>
2023-08-24 23:07:50 +0700

    Create Common Base Favicons

--- src/BaseFavicons.php
+++ src/BaseFavicons.php
@@ -4,6 +4,28 @@ namespace OfficialXVIID\CI4Favicons;
 
 abstract class BaseFavicons
 {
+    /** 
+     * Input file
+     * 
+     * @var string 
+     */
+    protected string $input;
+
+    /** 
+     * Output folder
+     * 
+     * @var string 
+     */
+    protected string $output;
+
+
+    /** 
+     * Set input file
+     */
     abstract public function setInputFile(string $file);
-    abstract public function setOutputFolder(string $file);
+
+    /** 
+     * Set output folder
+     */
+    abstract public function setOutputFolder(string $folder);
 }
