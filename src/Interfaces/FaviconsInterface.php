d53ef4784cece13ef917e355662affb613180409
XVIID <developer@xviid.net>
2023-08-25 00:44:48 +0700

    Update Favicons Interface

--- src/Interfaces/FaviconsInterface.php
+++ src/Interfaces/FaviconsInterface.php
@@ -2,8 +2,36 @@
 
 namespace OfficialXVIID\CI4Favicons\Interfaces;
 
+use OfficialXVIID\CI4Favicons\Exceptions\FaviconsException;
+
 interface FaviconsInterface
 {
-    public function inputFile();
-    public function outputFolder();
+    /** 
+     * Input File 
+     */
+    public function inputFile(): string;
+
+    /** 
+     * Output Folder
+     */
+    public function outputFolder(): string;
+
+    /**
+     * Return sizes from options
+     * 
+     * @param bool $noOldApple exclude old Apple touch image sizes
+     * @param bool $noAndroid exclude manifest.json file and images for Androids
+     * @param bool $noMs exclude images for Windows and IE11 
+     * @return array
+     */
+    public static function getSizes($noOldApple = false, $noAndroid = false, $noMs = false): array;
+
+    /**
+     * Get the settings for Windows tile image size
+     * 
+     * @param type $name
+     * @return array
+     * @throws FaviconsException
+     */
+    public static function getTileSettings($name): array;
 }
