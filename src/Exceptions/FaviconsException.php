6c2cd5e201cf7b9338b3a739e235f7323cff3d08
XVIID <developer@xviid.net>
2023-08-25 00:15:19 +0700

    Update Favicons Exception

--- src/Exceptions/FaviconsException.php
+++ src/Exceptions/FaviconsException.php
@@ -6,8 +6,19 @@ use CodeIgniter\Exceptions\ExceptionInterface;
 
 class FaviconsException extends \RuntimeException implements ExceptionInterface
 {
+    /**
+     * For Failed To Load Imagine 
+     */
     public static function forFailedToLoadImagine()
     {
         return new static(lang('Favicons.failedToLoadImagine'));
+    }
+
+    /**
+     * Invalid Image Name 
+     */
+    public static function invalidImageName()
+    {
+        return new static(lang('Favicons.invalidImageName'));
     }
 }
