c1119045dcd33bbf6af1f6d4b84ffe1c61082c34
XVIID <developer@xviid.net>
2023-08-25 00:37:28 +0700

    Create Favicon Language Setting - lang:en

---
+++ src/Language/en/Favicons.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OfficialXVIID\CI4Favicons\Language\en;
+
+/** 
+ * Favicon Language Setting
+ * 
+ * @author  Dyntara XVIID <dyntara@teams.xviid.net>
+ * @locale  en 
+ */
+
+return [
+    "failedToLoadImagine"   => "the `imagick` or `gd` extension must be loaded to generate image",
+    "invalidImageName"      => "invalid image name",
+];
