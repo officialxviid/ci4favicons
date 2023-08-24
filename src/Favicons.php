877e11c0e88b4e12a12c5bac47650cf61c316d6c
XVIID <developer@xviid.net>
2023-08-24 23:29:40 +0700

    Update Common Favicons

--- src/Favicons.php
+++ src/Favicons.php
@@ -4,13 +4,43 @@ namespace OfficialXVIID\CI4Favicons;
 
 class Favicons extends BaseFavicons
 {
+    /** 
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /** 
+     * Set input file 
+     */
     public function setInputFile(string $file)
     {
         $this->input = $file;
     }
 
+    /** 
+     * Set output folder
+     */
     public function setOutputFolder(string $folder)
     {
         $this->output = $folder;
+    }
+
+    /** 
+     * Get input file 
+     */
+    public function inputFile()
+    {
+        return $this->input;
+    }
+
+    /** 
+     * Get output folder
+     */
+    public function outputFolder()
+    {
+        return $this->output;
     }
 }
