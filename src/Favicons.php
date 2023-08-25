c201d07a6d3eed5f7debb8d40522ee88a461851f
XVIID <developer@xviid.net>
2023-08-25 15:23:47 +0700

    Update Common Favicons

--- src/Favicons.php
+++ src/Favicons.php
@@ -2,10 +2,7 @@
 
 namespace OfficialXVIID\CI4Favicons;
 
-use OfficialXVIID\CI4Favicons\Exceptions\FaviconsException;
-use OfficialXVIID\CI4Favicons\Interfaces\FaviconsInterface;
-
-class Favicons extends BaseFavicons implements FaviconsInterface
+class Favicons extends BaseFavicons
 {
     /** 
      * Constructor
@@ -47,52 +44,18 @@ class Favicons extends BaseFavicons implements FaviconsInterface
         return $this->output;
     }
 
-    /**
-     * Return sizes from options
-     * 
-     * @param bool $noOldApple exclude old Apple touch image sizes
-     * @param bool $noAndroid exclude manifest.json file and images for Androids
-     * @param bool $noMs exclude images for Windows and IE11 
-     * @return array
+    /** 
+     * Generate 
      */
-    public static function getSizes($noOldApple = false, $noAndroid = false, $noMs = false): array
+    public function generate()
     {
-        $result = array_merge(parent::$sizes, array());
-        if ($noOldApple) {
-            unset($result['apple-touch-icon-57x57.png']);
-            unset($result['apple-touch-icon-60x60.png']);
-            unset($result['apple-touch-icon-72x72.png']);
-            unset($result['apple-touch-icon-114x114.png']);
-        }
-        if ($noAndroid) {
-            unset($result['android-chrome-36x36.png']);
-            unset($result['android-chrome-48x48.png']);
-            unset($result['android-chrome-72x72.png']);
-            unset($result['android-chrome-96x96.png']);
-            unset($result['android-chrome-144x144.png']);
-        }
-        if ($noMs) {
-            unset($result['mstile-70x70.png']);
-            unset($result['mstile-144x144.png']);
-            unset($result['mstile-150x150.png']);
-            unset($result['mstile-310x310.png']);
-            unset($result['mstile-310x150.png']);
-        }
-        return $result;
-    }
+        // Checking
+        $this->checkInputFile();
+        $this->checkOutputFolder();
 
-    /**
-     * Get the settings for Windows tile image size
-     * 
-     * @param type $name
-     * @return array
-     * @throws \RuntimeException
-     */
-    public static function getTileSettings($name): array
-    {
-        if (!isset(parent::$tileSettings[$name])) {
-            throw FaviconsException::forInvalidImageName();
-        }
-        return parent::$tileSettings[$name];
+        // Generate
+        $this->generateICO();
+        $this->generatePNGs();
+        $this->generateManifestJson();
     }
 }
