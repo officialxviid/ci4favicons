c1c1435c2fd770e420086e78908cc7b674d8742b
XVIID <developer@xviid.net>
2023-08-25 15:50:43 +0700

    Update Common Favicons

--- src/Favicons.php
+++ src/Favicons.php
@@ -2,6 +2,9 @@
 
 namespace OfficialXVIID\CI4Favicons;
 
+use CodeIgniter\CLI\CLI;
+use OfficialXVIID\CI4Favicons\Exceptions\FaviconsException;
+
 class Favicons extends BaseFavicons
 {
     /** 
@@ -49,9 +52,26 @@ class Favicons extends BaseFavicons
      */
     public function generate()
     {
-        // Checking
-        $this->checkInputFile();
-        $this->checkOutputFolder();
+        // Checking Input
+        if (!$this->checkInputFile()) {
+            throw FaviconsException::forInputFileNotExist();
+            exit(1);
+        }
+
+        // Checking Output
+        if (!$this->checkOutputFolder()) {
+            if (is_cli()) {
+                if (CLI::prompt(lang('Favicons.doYouWantToCreateOutputPath', [lang('Favicons.yes'), lang('Favicons.no')])) == lang('Favicons.yes')) {
+                    mkdir($this->output, 0755);
+                } else {
+                    throw FaviconsException::forOutputDirectoryNotExist();
+                    exit(1);
+                }
+            } else {
+                throw FaviconsException::forOutputDirectoryNotExist();
+                exit(1);
+            }
+        }
 
         // Generate
         $this->generateICO();
